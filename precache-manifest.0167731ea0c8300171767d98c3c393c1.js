b4ad3d6cec9686ae0f724e7145083752aa79b717
Gvind-hub <63408287+Gvind-hub@users.noreply.github.com>
2020-09-08 15:40:57 +0300

    Updates

---
+++ precache-manifest.0167731ea0c8300171767d98c3c393c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f75cdc1c5fcfc72736e8a4470e5162",
+    "url": "/react-pig-game/index.html"
+  },
+  {
+    "revision": "46976991232a743c7439",
+    "url": "/react-pig-game/static/css/main.0e8c9ba2.chunk.css"
+  },
+  {
+    "revision": "3bb7c609d18f2ae95c35",
+    "url": "/react-pig-game/static/js/2.31fd9e0a.chunk.js"
+  },
+  {
+    "revision": "d8d5205a705bc8a565fce90323ae5387",
+    "url": "/react-pig-game/static/js/2.31fd9e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46976991232a743c7439",
+    "url": "/react-pig-game/static/js/main.d5958f98.chunk.js"
+  },
+  {
+    "revision": "e67f8729d9c4b91eb559",
+    "url": "/react-pig-game/static/js/runtime-main.36017952.js"
+  },
+  {
+    "revision": "4b371f162c2d48d284b8371ac430e85b",
+    "url": "/react-pig-game/static/media/loader.4b371f16.svg"
+  }
+]);
